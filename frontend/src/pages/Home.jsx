07044aa7115f6a25a1b81856a531dceeba28f156
aditikantharia <aditikantharia@gmail.com>
2026-04-17 13:20:36 +0530

    add frontend

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <>
+    <div className='flex items-center justify-center w-full h-screen'>
+        <Link to="/login">
+            <button className='w-full bg-black text-white p-2
+             text-center'>got ot login page</button>
+       </Link> 
+    </div>
+    </>
+  )
+}
+
+export default Home
